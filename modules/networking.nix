d0397f5bbb8decf87cc61b29f68ff770ae750eb0
cleran <cleran.brulius@gmail.com>
2026-03-03 12:36:08 +0100

    added networkmanagerapplet
    contains the package nm-connection-editor as a gui for more advanced
    network management

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,8 +1,12 @@
-{lib,hostName,...}: {
+{lib,hostName,pkgs,...}: {
 
   programs.mtr.enable = true; # network diagnostics
 
   networking.hostName = lib.mkDefault "${hostName}"; # Define your hostname.
   # Enable networking
   networking.networkmanager.enable = true;
+  
+  environment.systemPackages = [
+    pkgs.networkmanagerapplet
+  ];
 }
