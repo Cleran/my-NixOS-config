6b3833dedc3e988e91597f3308a85aec0c41b522
cleran <cleran.brulius@gmail.com>
2026-04-27 15:03:48 +0200

    added TODO list, disabled bluetooth on boot

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+- nix-ld: run scripts and binaries that need dynamic libs
+- project switcher: workflow improvement
+    - automatically create and reattach zellij sessions
+    - fuzzy find projects
--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -3,7 +3,7 @@
 {
   hardware.bluetooth = {
     enable = true;
-    powerOnBoot = true;
+    powerOnBoot = false;
     settings = {
       General = {
         # Shows battery charge of connected devices on supported
